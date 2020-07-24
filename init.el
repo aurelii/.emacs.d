e573eca908e71043e877c3e9d7f457517930297a
Kamil <51153571+kamilbeker@users.noreply.github.com>
2020-07-24 16:06:40 +0200

    Add temporary fix for working with remote server

--- init.el
+++ init.el
@@ -10,13 +10,36 @@
 (package-initialize)
 ;; Dirty fix for signature check failure
 (setq package-check-signature nil)
-(package-refresh-contents)
-;; Setup package autoloads, keybindings, and various mode configuration
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
+
+(defun set-use-packages ()
+  "Setup used packages"  
+  ;; auto-complete
+  (use-package auto-complete
+	       :config
+	       (ac-config-default)
+	       (setq ac-disable-faces nil)
+	       (global-auto-complete-mode t))
+  ;; git-gutter setup
+  (use-package git-gutter
+	       :config
+	       (global-git-gutter-mode t)
+	       ;; If you would like to use git-gutter.el and linum-mode
+	       (git-gutter:linum-setup)
+	       ;; If you enable git-gutter-mode for some modes
+	       (global-set-key (kbd "C-x C-g") 'git-gutter)
+	       (global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+	       ;; Jump to next/previous hunk
+	       (global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+	       (global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+	       ;; Stage current hunk
+	       (global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+	       ;; Revert current hunk
+	       (global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+	       ;; Mark current hunk
+	       (global-set-key (kbd "C-x v SPC") #'git-gutter:mark-hunk)))
 
 (defun init ()
+  "Initialize basic config"
   ;; Disable startup screen
   (setq inhibit-startup-screen t)
   ;; Disable tool-bar
@@ -44,31 +67,17 @@
 ;; initialize hooks
 (add-hook 'after-save-hook #'shell-format-on-save)
 
-;; auto-complete
-(use-package auto-complete
-  :config
-  (ac-config-default)
-  (setq ac-disable-faces nil)
-  (global-auto-complete-mode t))
-
-;; git-gutter setup
-(use-package git-gutter
-  :config
-  (global-git-gutter-mode t)
-  ;; If you would like to use git-gutter.el and linum-mode
-  (git-gutter:linum-setup)
-  ;; If you enable git-gutter-mode for some modes
-  (global-set-key (kbd "C-x C-g") 'git-gutter)
-  (global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
-  ;; Jump to next/previous hunk
-  (global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
-  (global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
-  ;; Stage current hunk
-  (global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
-  ;; Revert current hunk
-  (global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
-  ;; Mark current hunk
-  (global-set-key (kbd "C-x v SPC") #'git-gutter:mark-hunk)
-) 
+(if (string-match "waw" system-name)
+    (progn
+      ;; Load packages manually
+      ;; TODO: Load manually all packages that are stored in elpa_backup directory
+      (message "Remote Server Setup"))
+  (progn
+    ;; Setup package autoloads, keybindings, and various mode configuration
+    (package-refresh-contents)
+    (unless (package-installed-p 'use-package)
+      (package-install 'use-package))
+    (require 'use-package)
+    (set-use-packages)))
 
 (init)
